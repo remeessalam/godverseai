c9cab6d89162b9fab005ed7dbb5ae102f5d5a68a
remeessalam <remeesreme4u@gmail.com>
2025-03-22 18:44:15 +0530

    all completed

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../Components/ScrollToTop";
 
 const AppLayout = () => {
   return (
     <div>
       <Header />
+      <ScrollToTop />
       <div className="bg-white dark:bg-darkbackground">
         <Outlet />
       </div>
--- src/util/helper.js
+++
@@ -1,6 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-const navigate = useNavigate();
-export const navigateTo = (link) => {
-  navigate(link);
-};
