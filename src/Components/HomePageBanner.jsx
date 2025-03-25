ec70ee1428c4280b73965069a2a85b8b493e0411
remeessalam <remeesreme4u@gmail.com>
2025-03-25 19:47:50 +0530

    removed code

--- src/Components/HomePageBanner.jsx
+++ src/Components/HomePageBanner.jsx
@@ -16,17 +16,6 @@ const HomePageBanner = () => {
   const [headingComplete, setHeadingComplete] = useState(false);
   const [cursorVisible, setCursorVisible] = useState(true);
   const [cursorPosition, setCursorPosition] = useState("heading");
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    checkMobile();
-    window.addEventListener("resize", checkMobile);
-    return () => window.removeEventListener("resize", checkMobile);
-  }, []);
 
   useEffect(() => {
     // Type out the heading text
